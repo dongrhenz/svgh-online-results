83a95640345c372321d7e9ff0b4bb383423167df
dongrhenz <45716207+dongrhenz@users.noreply.github.com>
2026-09-10 08:38:41 +0800

    Test index.php automatic deployment

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
-<html lang="en"><!-- Deployment test -->
+<html lang="en">
+<!-- Deployment test -->
+<!-- TEST 2: GitHub to cPanel deployment -->
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
